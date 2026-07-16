097d8ef100fdbbf6edf776300898eed9aa5ac2ea
iwandwip <mcclawsher369@gmail.com>
2026-07-16 12:27:55 +0700

    refactor(cycle): restructure updateCycle with full production metrics
    Expand the cycle update routine to read output, quota, ISI and NG
    values from the HMI and validate ISI and quota constraints before
    incrementing cycle counts. Derived OK counts and production totals
    are now written back and pushed to the simulation and dashboard
    displays for accurate real-time tracking.

--- firmware/CaesarFirmwareV1/cycle.ino
+++ firmware/CaesarFirmwareV1/cycle.ino
@@ -16,9 +16,50 @@ void updateCycle() {
   if (machineReadyKnown && machineReady == 1 && previousMachineReady == 0) {
     uint32_t frontCycle = 0;
     uint32_t backCycle = 0;
-    if (readNextionValue("pageSys.nFCyc", &frontCycle) && readNextionValue("pageSys.nBCyc", &backCycle)) {
-      sendInputValue("pageSys.nFCyc", frontCycle + 1);
-      sendInputValue("pageSys.nBCyc", backCycle + 1);
+    uint32_t frontOutput = 0;
+    uint32_t backOutput = 0;
+    uint32_t frontQuota = 0;
+    uint32_t backQuota = 0;
+    uint32_t frontIsi = 0;
+    uint32_t backIsi = 0;
+    uint32_t frontNg = 0;
+    uint32_t backNg = 0;
+
+    if (readNextionValue("pageSys.nFCyc", &frontCycle) &&
+        readNextionValue("pageSys.nBCyc", &backCycle) &&
+        readNextionValue("pageSys.nFOut", &frontOutput) &&
+        readNextionValue("pageSys.nBOut", &backOutput) &&
+        readNextionValue("pageSys.nFQuota", &frontQuota) &&
+        readNextionValue("pageSys.nBQuota", &backQuota) &&
+        readNextionValue("pageSys.nFIsi", &frontIsi) &&
+        readNextionValue("pageSys.nBIsi", &backIsi) &&
+        readNextionValue("pageSys.nFNG", &frontNg) &&
+        readNextionValue("pageSys.nBNG", &backNg) &&
+        frontIsi > 0 && backIsi > 0 &&
+        frontQuota >= frontIsi && backQuota >= backIsi) {
+      frontCycle++;
+      backCycle++;
+      frontOutput += frontIsi;
+      backOutput += backIsi;
+      frontQuota -= frontIsi;
+      backQuota -= backIsi;
+      uint32_t frontOk = frontOutput - frontNg;
+      uint32_t backOk = backOutput - backNg;
+
+      sendInputValue("pageSys.nFCyc", frontCycle);
+      sendInputValue("pageSys.nBCyc", backCycle);
+      sendInputValue("pageSys.nFOut", frontOutput);
+      sendInputValue("pageSys.nBOut", backOutput);
+      sendInputValue("pageSys.nFQuota", frontQuota);
+      sendInputValue("pageSys.nBQuota", backQuota);
+      sendInputValue("pageSys.nFOK", frontOk);
+      sendInputValue("pageSys.nBOK", backOk);
+      updateFrontSimulationDisplay(frontCycle, frontOutput, frontQuota, frontIsi, frontNg, frontOk);
+      updateBackSimulationDisplay(backCycle, backOutput, backQuota, backIsi, backNg, backOk);
+      updateFrontDashboardDisplay(frontCycle, frontOutput, frontIsi);
+      updateBackDashboardDisplay(backCycle, backOutput, backIsi);
+      sendInputValue("pageDashboard.nFNgD", frontNg);
+      sendInputValue("pageDashboard.nBNgD", backNg);
     }
   }
 
